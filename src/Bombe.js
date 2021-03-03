f711f411365b82b4a55af0e552d88d356d67fc38
Mohamed <m.desoki@hotmail.fr>
2021-03-03 09:55:12 +0100

    Créer class Bombe.js

---
+++ src/Bombe.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+export class Bombe extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { minutes:3,secondes:59,explosion:false};
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h2>Tic Tac ....</h2>
+                <input disabled type="texte" value={this.state.minutes+":"+this.state.secondes}/>
+            </div>
+        );
+    }
+    componentDidMount=()=> {
+        let numSeconde=this.state.secondes;
+        let idTimer=setInterval( ()=>{
+            this.setState({secondes: --numSeconde})
+            },1000)
+        this.setState({timer:idTimer})
+    }
+}
